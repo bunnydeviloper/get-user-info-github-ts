040e4c7dfc25e7c8c2327c331de6873f5e5d5bda
Sophia Mai <phuongmai001@gmail.com>
2018-12-31 17:37:08 -0800

    add cb within cb (cb hell) to modify user obj

--- out/index.js
+++ out/index.js
@@ -1,10 +1,20 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var GithubApiService_1 = require("./GithubApiService");
+var _ = __importStar(require("lodash"));
 var svc = new GithubApiService_1.GithubApiService();
 svc.getUserInfo('bunnydeviloper', function (user) {
-    console.log(user);
-});
-svc.getRepos('bunnydeviloper', function (repos) {
-    console.log(repos);
+    svc.getRepos('bunnydeviloper', function (repos) {
+        var sortedRepos = _.sortBy(repos, [function (repo) { return repo.size * -1; }]);
+        // note: *-1 so we sort highest -> lowest
+        var filteredRepos = _.take(sortedRepos, 3);
+        user.repos = filteredRepos;
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { GithubApiService } from './GithubApiService';
 import { User } from './User';
 import { Repo } from './Repo';
+import * as _ from 'lodash';
 
 let svc = new GithubApiService();
 svc.getUserInfo('bunnydeviloper', (user: User) => {
-  console.log(user);
-});
-svc.getRepos('bunnydeviloper', (repos: Repo[]) => {
-  console.log(repos);
+  svc.getRepos('bunnydeviloper', (repos: Repo[]) => {
+    let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.size * -1]);
+    // note: *-1 so we sort highest -> lowest
+    let filteredRepos = _.take(sortedRepos, 3);
+    user.repos = filteredRepos;
+  });
 });
